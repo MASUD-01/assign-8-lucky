b6f910c1503cc347972cb02cf3ce7b7acc7cbc0d
MASUD-01 <masudrana96141@gmail.com>
2022-03-26 00:25:11 +0600

    end question and ans

--- src/components/Quens-and-ans/Ans.js
+++ src/components/Quens-and-ans/Ans.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Ans = () => {
     return (
         <div>
+            <h2>Q:How React works?</h2>
+            <p>Ans:react works unidirectional data flow. react creates its own virtual dom.if need any changes react compare to virtual DOM and Real DOM. Then adjectly where need changes,react do that immidiatley.</p>
 
+            <h2>Q:Props Vs state?</h2>
+            <p>Ans:State is the local state of the component which cannot  access and modified outside of the component. It's related to local variables in a function. Props make components reusable by giving components the ability to receive data from their parent component in the form of props.</p>
         </div>
     );
 };
